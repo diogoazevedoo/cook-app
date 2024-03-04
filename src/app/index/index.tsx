61fb3ccb480d023214d161f0475efbcfc28141a2
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-03-04 23:10:53 +0000

    UI & Config: config theme and fonts using _layout and created first ui for the index screen

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,9 +1,18 @@
 import { View, Text } from "react-native";
 
+import { styles } from "./styles";
+
 export default function Index() {
     return (
-        <View>
-            <Text>Hello World!</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>
+                Choose {"\n"}
+                <Text style={styles.subtitle}>the products</Text>
+            </Text>
+
+            <Text style={styles.message}>
+                Discover recipes based on the products you've chosen
+            </Text>
         </View>
     )
 }
