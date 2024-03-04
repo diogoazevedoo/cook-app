06828d3df0f8e83601cff41a6ba5ab55782f6495
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-03-04 23:49:40 +0000

    UI & Feat: first implementation of choose product list with select and unselect feature on click

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,8 +1,22 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
+import { useState } from "react";
 
 import { styles } from "./styles";
 
+import { Ingredient } from "@/components/Ingredient";
+
+
 export default function Index() {
+    const [selected, setSelected] = useState<string[]>([])
+
+    function handleToggleSelected(value: string) {
+        if(selected.includes(value)) {
+            return setSelected((state) => state.filter((item) => item !== value))
+        }
+
+        setSelected((state) => [...state, value])
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>
@@ -13,6 +27,21 @@ export default function Index() {
             <Text style={styles.message}>
                 Discover recipes based on the products you've chosen
             </Text>
+
+            <ScrollView 
+                contentContainerStyle={styles.ingredients} 
+                showsVerticalScrollIndicator={false}
+            >
+                {Array.from({ length: 20 }).map((item, index) => (
+                    <Ingredient 
+                        key={index} 
+                        name="Tomato" 
+                        image="" 
+                        selected={selected.includes(String(index))} 
+                        onPress={() => handleToggleSelected(String(index))} 
+                    />
+                ))}
+            </ScrollView>
         </View>
     )
 }
